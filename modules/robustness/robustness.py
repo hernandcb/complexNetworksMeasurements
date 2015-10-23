db5e6e4087a6e5a4be415d146ca051aadd6a1be3
hernandcb <carvajal.hernandavid@gmail.com>
2015-10-23 12:13:46 -0500

    Improving the robustness script to use only the networkx library

--- modules/robustness/robustness.py
+++ modules/robustness/robustness.py
@@ -20,11 +20,40 @@ centrality = {
 }
 
 
+def average_path_length():
+    None
+
+
+def average_shortest_path_lengthNK(g):
+    """
+    TODO - Specify that this method also works with disconnected networks
+
+    """
+    n = g.numberOfNodes()
+    avg = 0.0
+
+    for node in g.nodes():
+        bfs = nk.graph.BFS(g, node).run()
+        avg = sum(filter(lambda a: a != sys.float_info.max, bfs.getDistances()))
+
+    return avg / (n*(n-1))
+
+
 def largest_component_size(g):
     return len(sorted(nx.connected_components(g), key=len, reverse=True)[0])
 
 
-def calculate(g, strategy="degree", sequential=True):
+def largest_component_sizeNK(g):
+    components_size = nk.properties.components(nk.nx2nk(G))[1]
+    return sorted(components_size.items(), key=operator.itemgetter(1), reverse=True)[0][1]
+
+comparative_measures = {
+    "component_size": largest_component_size # ,
+    # "path_length": average_path_length
+}
+
+
+def calculateNK(g, strategy="degree", measure="component_size", sequential=True):
     """
     This method calculates the robustness index of the network by removing the
     nodes from the network and comparing the size of the largest component in
@@ -39,35 +68,85 @@ def calculate(g, strategy="degree", sequential=True):
     Return
     ----------
     vertices_removed: A list with the fraction of vertices removed
-    component_size:   A list with the component size given a fraction of the
-                      network removed
+    comparative_measure_values:   A list with the comparative measure values
+                          (i.e. component size or avg path length) given a
+                          fraction of the network removed
     robustness_index: The robustness index value
     """
+    vertices_removed = []
+    comparative_measure_values = []
+    n = len(g.nodes())
+    r = 0.0
+    rank = ranking(g, strategy)
+    vertices_removed.append(0)
+    comparative_measure_values.append(comparative_measures[measure](g)/n)
 
+    for i in range(1, n):
+        g.remove_node(rank.pop(0))
+        comparative_value = comparative_measures[measure](g)
+        r +=  comparative_value / n
+
+        # print("vr: {}, cs: {}".format(i/n, largest_component_size(g)/n))
+        vertices_removed.append(i / n)
+        comparative_measure_values.append(comparative_value / n)
+
+        if not sequential:
+            rank = ranking(g, strategy)
+
+    return vertices_removed, comparative_measure_values, (r / n)
+
+
+def calculate(g, strategy="degree", measure="component_size", sequential=True):
+    """
+    This method calculates the robustness index of the network by removing the
+    nodes from the network and comparing the size of the largest component in
+    the network to the number of nodes removed.
+
+    Params
+    ---------
+    g:          networkx graph
+    sequential: when false the ranking is updated each time a node is removed.
+    strategy:   The strategy used to remove the nodes
+
+    Return
+    ----------
+    vertices_removed: A list with the fraction of vertices removed
+    comparative_measure_values:   A list with the comparative measure values
+                          (i.e. component size or avg path length) given a
+                          fraction of the network removed
+    robustness_index: The robustness index value
+    """
     # Use networkx graph instead of networkit
     if type(g) is nk.Graph:
         g = nk.nk2nx(g)
 
     vertices_removed = []
-    component_size = []
+    comparative_measure_values = []
     n = len(g.nodes())
     r = 0.0
     rank = ranking(g, strategy)
     vertices_removed.append(0)
-    component_size.append(largest_component_size(g)/n)
+    comparative_measure_values.append(comparative_measures[measure](g)/n)
 
     for i in range(1, n):
+        print("----------------------------------------------")
+        print(g.nodes())
+        print(rank)
+        print("Sequential: ", sequential)
+        print("Measure: ", measure)
+        print("----------------------------------------------")
         g.remove_node(rank.pop(0))
-        r += largest_component_size(g) / n
+        comparative_value = comparative_measures[measure](g)
+        r +=  comparative_value / n
 
         # print("vr: {}, cs: {}".format(i/n, largest_component_size(g)/n))
         vertices_removed.append(i / n)
-        component_size.append(largest_component_size(g) / n)
+        comparative_measure_values.append(comparative_value / n)
 
         if not sequential:
             rank = ranking(g, strategy)
 
-    return vertices_removed, component_size, (r / n)
+    return vertices_removed, comparative_measure_values, (r / n)
 
 
 def ranking(gx, measure="degree", reverse=False):
@@ -111,39 +190,45 @@ def plot_robustness_analysis(g, debug=True):
     ---------
     g: Networkit graph
     """
-    pylab.figure(1, dpi=500)
-    pylab.xlabel(r"Fraction of vertices removed ($\rho$)")
-    pylab.ylabel(r"Fractional size of largest component ($\sigma$)")
 
-    # Color generator
-    color = iter(pylab.cm.rainbow(np.linspace(0, 1, len(centrality.keys()))))
+    for name, comparative_measure in comparative_measures.items():
+        for sequential_analysis in [False, True]:
+            current_time = time.strftime("%d-%m-%Y_%H%M%S")
+            file_name = g.getName() + "_robustness_" + name + "_"
+            file_name += "simultaneous" if sequential_analysis else "sequential"
+            file_name += "_" + current_time
+            print(file_name)
+            if debug:
+                file_results = open(file_name + ".results", 'a')
 
-    current_time = time.strftime("%d-%m-%Y_%H%M%S")
-    file_name = g.getName() + "_robustness_" + current_time
+            pylab.figure(1, dpi=500)
+            pylab.xlabel(r"Fraction of vertices removed ($\rho$)")
+            pylab.ylabel(r"Fractional size of largest component ($\sigma$)")
 
-    if debug:
-        file_results = open(file_name + ".results", 'a')
+            # Color generator
+            color = iter(pylab.cm.rainbow(np.linspace(0, 1, len(centrality.keys()))))
 
-    for strategy in centrality.keys():
-        vertices_removed, component_size, r_index = calculate(g, strategy)
-        label = "%s ($R = %4.3f$)" % (strategy, r_index)
-        pylab.plot(vertices_removed, component_size, label=label, c=next(color), alpha=0.6, linewidth=2.0)
+            for strategy in centrality.keys():
+                vertices_removed, component_size, r_index = calculate(g, strategy, name, sequential_analysis)
+                label = "%s ($R = %4.3f$)" % (strategy, r_index)
+                pylab.plot(vertices_removed, component_size, label=label, c=next(color), alpha=0.6, linewidth=2.0)
 
-        if debug:
-            print("{} {}".format(strategy, r_index), file=file_results)
+                if debug:
+                    print("{} {}".format(strategy, r_index), file=file_results)
+
+            pylab.legend(loc="upper right", shadow=False)
+            pylab.savefig(file_name + ".pdf", format="pdf")
+            pylab.close(1)
 
     if debug:
         file_results.close()
 
-    pylab.legend(loc="upper right", shadow=False)
-    pylab.savefig(file_name + ".pdf", format="pdf")
-    pylab.close(1)
-
-
 if __name__ == "__main__":
     # graph = nx.read_graph6("football.graph6")
     # graph = nx.erdos_renyi_graph(100, 1)
-    erg = nk.generators.ErdosRenyiGenerator(100, 0.3, False)
+
+
+    erg = nk.generators.ErdosRenyiGenerator(10, 0.3, False)
     graph = erg.generate()
 
     plot_robustness_analysis(graph)
